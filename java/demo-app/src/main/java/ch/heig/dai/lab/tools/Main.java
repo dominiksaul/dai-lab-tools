1738966a346de74fc45f92207adcb436f0fa7f0a
Dominik Saul <dominik@saul.ch>
2023-09-22 18:59:24 +0200

    changes dsa

--- java/demo-app/src/main/java/ch/heig/dai/lab/tools/Main.java
+++ java/demo-app/src/main/java/ch/heig/dai/lab/tools/Main.java
@@ -2,6 +2,12 @@ package ch.heig.dai.lab.tools;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        while (true) {
+            System.out.println("Hello world!");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+        }
     }
 }
